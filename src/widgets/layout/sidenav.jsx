d4aa3e5797552c0f1bb32d7f8ab0771c287b74bc
thatkhay <johnkingsleyegeonu@gmail.com>
2024-07-21 20:19:03 +0100

    b

--- src/widgets/layout/sidenav.jsx
+++ src/widgets/layout/sidenav.jsx
@@ -65,7 +65,7 @@ export function Sidenav({ brandImg, brandName, routes }) {
               <li key={name}>
                 <NavLink to={`/${layout}${path}`}>
                   {({ isActive }) => (
-                    <Button style={{backgroundColor: 'trasparent'}}
+                    <Button style={{backgroundColor: 'white'}}
                       variant={isActive ? "gradient" : "text"}
                       color={
                         isActive
