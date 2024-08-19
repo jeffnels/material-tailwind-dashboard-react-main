cd70541dd0906daf8dbd3aab7789a8e565908ad7
thatkhay <johnkingsleyegeonu@gmail.com>
2024-08-19 13:30:34 +0100

    fix

--- src/pages/dashboard/home.jsx
+++ src/pages/dashboard/home.jsx
@@ -4,21 +4,9 @@ import {
   Card,
   CardHeader,
   CardBody,
-  IconButton,
-  Menu,
-  MenuHandler,
-  MenuList,
-  MenuItem,
 } from "@material-tailwind/react";
-import {
-  EllipsisVerticalIcon,
-  CheckCircleIcon,
-  ClockIcon,
-  ArrowUpIcon,
-} from "@heroicons/react/24/outline";
 import { StatisticsCard } from "@/widgets/cards";
 import { StatisticsChart } from "@/widgets/charts";
-
 import {
   statisticsCardsData,
   statisticsChartsData,
@@ -29,10 +17,9 @@ import NoTransactionComponent from "@/components/NoTransactionComponent";
 import KycButton from "@/components/KycButton";
 import WithdrawalButton from "@/components/WithdrawalButton";
 import DepositButton from "@/components/DepositButton";
-import { Money } from "@phosphor-icons/react/dist/ssr";
+import { Money } from "@phosphor-icons/react";
 import { Coin, CreditCard, StackPlus } from "@phosphor-icons/react";
-// import { CreditCard, Money, StackPlus, TreasureChest } from "@phosphor-icons/react";
-// import { Coins } from "@phosphor-icons/react/dist/ssr";
+import reloadButton from '../../images/reload.png';
 
 export function Home() {
   const [projectsTableData, setProjectsTableData] = useState([]);
@@ -41,22 +28,44 @@ export function Home() {
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState(10);
   const [isVerified, setIsVerified] = useState(0);
-  const navigate = useNavigate();
   const [amount, setAmount] = useState(0);
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    // Fetch user amount from localStorage
-    const userDetails = JSON.parse(localStorage.getItem("user"));
-    if (userDetails) {
-      console.log("User details from localStorage:", userDetails);
+  const fetchUserInfo = async () => {
+    try {
+      const authToken = localStorage.getItem("authToken");
+      if (!authToken) {
+        navigate("/auth/sign-in");
+        setAuthenticated(false);
+        return;
+      }
+
+      const response = await fetch(
+        "https://tradesphere-backend.onrender.com/api/users/me",
+        {
+          method: "GET",
+          headers: {
+            "x-auth-token": authToken,
+          },
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch user info");
+      }
+      const userDetails = await response.json();
+      console.log("Fetched user details:", userDetails);
       if (userDetails.amount) {
         setAmount(userDetails.amount);
-        console.log("User amount:", userDetails.amount);
       }
       setIsVerified(userDetails.isVerified);
-    } else {
-      console.warn("No user details found in localStorage");
+      localStorage.setItem("user", JSON.stringify(userDetails)); // Update localStorage with the new details
+    } catch (error) {
+      console.error("Error fetching user info", error);
     }
+  };
+
+  useEffect(() => {
+    fetchUserInfo();
 
     const fetchData = async () => {
       setLoading(true);
@@ -108,21 +117,25 @@ export function Home() {
 
   return (
     <div className="mt-12">
-      <div className="flex  gap-4 ">
+      <div className="flex  gap-4  ">
 
         <WithdrawalButton />
         <DepositButton />
+        <img 
+          src={reloadButton} 
+          alt="Reload"
+          className="h-[24px] w-[24px] mt-2 cursor-pointer mr-3"
+          onClick={fetchUserInfo} // Fetch updated user info on reload
+        />
       </div>
-      {/* <Money size={32} /> */}
       <div className="mb-12 grid gap-y-10 gap-x-6 md:grid-cols-2 lg:grid-cols-4">
         <StatisticsCard
           key="amount"
           title="Balance"
           value={amount}
-        
           icon={React.createElement(Money, {
-             className: "w-6 h-6 text-white",
-           })}
+            className: "w-6 h-6 text-white",
+          })}
           footer={
             <Typography className="font-normal text-blue-gray-600">
               <strong className="text-green-500">Up</strong> since last month
@@ -145,21 +158,21 @@ export function Home() {
         />
 
         <StatisticsCard
-          key="amount"
-       
+          key="profits"
           title="Profits"
           value={0}
           icon={React.createElement(StackPlus, {
             className: "w-6 h-6 text-white",
-           })}
+          })}
           footer={
             <Typography className="font-normal text-blue-gray-600">
               <strong className="text-green-500">Up</strong> since last month
             </Typography>
           }
         />
+
         <StatisticsCard
-          key="amount"
+          key="investedAmount"
           title="Invested Amount"
           value={0}
           icon={React.createElement(CreditCard, {
@@ -171,23 +184,8 @@ export function Home() {
             </Typography>
           }
         />
-        {/* {statisticsCardsData.map(({ icon, title, footer, ...rest }) => (
-          <StatisticsCard
-            key={title}
-            {...rest}
-            title={title}
-            icon={React.createElement(icon, {
-              className: "w-6 h-6 text-white",
-            })}
-            footer={
-              <Typography className="font-normal text-blue-gray-600">
-                <strong className={footer.color}>{footer.value}</strong>{" "}
-                {footer.label}
-              </Typography>
-            }
-          />
-        ))} */}
       </div>
+
       <div className="mb-4 grid grid-cols-1 gap-6 xl:grid-cols-3">
         <Card className="overflow-hidden col-span-3 border border-blue-gray-100 shadow-sm">
           <CardHeader
@@ -201,18 +199,6 @@ export function Home() {
                 Transactions
               </Typography>
             </div>
-            {/* <Menu placement="left-start">
-              <MenuHandler>
-                <IconButton size="sm" variant="text" color="blue-gray">
-                  <EllipsisVerticalIcon
-                    strokeWidth={3}
-                    fill="currenColor"
-                    className="h-6 w-6"
-                  />
-                </IconButton>
-              </MenuHandler>
-
-            </Menu> */}
           </CardHeader>
           <CardBody className="overflow-x-scroll px-0 pt-0 pb-2">
             {loading ? (
@@ -251,11 +237,8 @@ export function Home() {
                         </tr>
                       ) : (
                         currentItems.map(({ 
-                          transactionsId
-                          , amount, type, package_type, created_at, status }) => (
-                          <tr key={
-                            transactionsId
-                            }>
+                          transactionsId, amount, type, package_type, created_at, status }) => (
+                          <tr key={transactionsId}>
                             <td className="py-3 px-5 border-b border-blue-gray-50 whitespace-nowrap">
                               <Typography variant="small" color="blue-gray">
                                 {transactionsId}
@@ -271,14 +254,13 @@ export function Home() {
                                 {type}
                               </Typography>
                             </td>
-                            
                             <td className="py-3 px-5 border-b border-blue-gray-50">
                               <Typography variant="small" color="blue-gray">
                                 {created_at}
                               </Typography>
                             </td>
                             <td className="py-3 px-5 border-b border-blue-gray-50 whitespace-nowrap">
-                              <Typography variant="small" color="blue-gray whitespace-nowrap">
+                              <Typography variant="small" color="blue-gray">
                                 {status}
                               </Typography>
                             </td>
@@ -289,21 +271,21 @@ export function Home() {
                   </table>
                 </div>
                 {/* Pagination Controls */}
-                <div className="flex flex-col items-center mt-4 w-full sm:flex-row sm:justify-between px-5 mb-2">
+                <div className="flex justify-center mt-4">
                   <button
-                    onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+                    className="px-4 py-2 mx-2 text-sm text-white bg-blue-gray-500 rounded disabled:opacity-50"
                     disabled={currentPage === 1}
-                    className="px-4 py-2 bg-black text-white rounded-md mb-2 sm:mb-0 text-sm"
+                    onClick={() => setCurrentPage(currentPage - 1)}
                   >
                     Previous
                   </button>
-                  <span className="flex items-center mb-2 sm:mb-0">
+                  <span className="px-4 py-2 mx-2 text-sm">
                     Page {currentPage} of {totalPages}
                   </span>
                   <button
-                    onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
+                    className="px-4 py-2 mx-2 text-sm text-white bg-blue-gray-500 rounded disabled:opacity-50"
                     disabled={currentPage === totalPages}
-                    className="px-4 py-2 bg-black text-white rounded-md text-sm"
+                    onClick={() => setCurrentPage(currentPage + 1)}
                   >
                     Next
                   </button>
