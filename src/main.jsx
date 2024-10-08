eeb29228b88a4fd5e292a02ffd4bc7775a5b49eb
jeffnels <jeffnelson.1012@gmail.com>
2024-10-08 01:11:23 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -16,12 +16,14 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@material-tailwind/react";
 import { MaterialTailwindControllerProvider } from "@/context";
 import "../public/css/tailwind.css";
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider>
         <MaterialTailwindControllerProvider>
+          <Analytics />
           <App />
         </MaterialTailwindControllerProvider>
       </ThemeProvider>
